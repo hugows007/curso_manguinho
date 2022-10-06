a46dda30e8ee03ba9a7dce395823c57f894b5f37
hugows007 <hugobezerrapimentel@gmail.com>
2022-10-05 21:09:46 -0300

    ensure REmoteAuthentication throws UnexpectedError on 200 with invalid data

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -105,4 +105,18 @@ void main() {
 
     expect(account.token, accessToken);
   });
+
+  test('Should throw UnexpectedError if HttpClient return 200 with invalid data', () async {
+    when(() =>
+        httpClient.request(
+            url: any(named: 'url'),
+            method: any(named: 'method'),
+            body: captureAny(named: 'body'))).thenAnswer((_) async => {
+      'invalid_key': 'invalid_value',
+    });
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
