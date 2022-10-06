d01ffac9c14e476224799598af22405070e24183
hugows007 <hugobezerrapimentel@gmail.com>
2022-10-05 22:10:55 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -19,10 +19,12 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
     await client.post(
       Uri.parse(url),
       headers: headers,
-      body: jsonEncode(body),
+      body: jsonBody,
     );
   }
 }
@@ -53,6 +55,15 @@ void main() {
               'accept': 'application/json',
             },
             body: '{"any_key":"any_value"}',
+          ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: uri.path, method: 'post');
+
+      verify(() => client.post(
+            any(),
+            headers: any(named: 'headers'),
           ));
     });
   });
