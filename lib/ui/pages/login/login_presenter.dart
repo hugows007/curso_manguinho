5ac332d9f3a36749c0e8e22d297c024229790a18
hugows007 <hugobezerrapimentel@gmail.com>
2022-10-18 16:12:22 -0300

    ensure StreamLoginPresenter calls Validation on email changed

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -5,7 +5,7 @@ abstract class LoginPresenter {
   Stream<bool> get isFormValidStream;
   Stream<bool> get isLoadingStream;
 
-  void validateEmail(String email);
+
   void validatePassword(String password);
   Future<void>? auth();
   void dispose();
