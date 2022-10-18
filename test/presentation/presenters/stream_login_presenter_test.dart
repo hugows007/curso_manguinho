a473b7ded17667eda3acce69f32db8ce9509ac93
hugows007 <hugobezerrapimentel@gmail.com>
2022-10-18 16:37:56 -0300

    ensure EmailErrorStream will not emit duplicated event

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -38,10 +38,9 @@ void main() {
   test('Should emit email error if validation fails', () {
     mockValidation(value: 'error');
 
-    expectLater(sut.emailErrorStream, emits('error'));
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, 'error')));
 
     sut.validateEmail(email);
-
-    verify(() => validation.validate(field: 'email', value: email)).called(1);
+    sut.validateEmail(email);
   });
 }
